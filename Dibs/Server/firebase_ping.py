036943191104993126193edd8e4be23a3f627dcc
Jacob Lattie <jhlattie@gatech.edu>
2020-01-23 12:21:18 -0500

    Firebase Database pod

--- Dibs/Server/firebase_ping.py
+++ Dibs/Server/firebase_ping.py
@@ -6,4 +6,4 @@ from firebase import firebase
 
 firebase = firebase.FirebaseApplication('https://dibs-a0ffe.firebaseio.com/')
 
-result = firebase.post('host id mentioned', {'cTemp':str(100)})
+result = firebase.post('chair_id', {'occupied':True})
