d365d711560d319badfe8298bb202e93efdf02d6
Jacob Lattie <jhlattie@gatech.edu>
2020-01-23 13:04:47 -0500

    Database GET from iOS

--- Dibs/iOS/Dibs/AppDelegate.swift
+++ Dibs/iOS/Dibs/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreData
 import Firebase
 
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let db = FireDatabaseController(ref: Database.database().reference())
+        db.read()
         return true
     }
 
---
+++ Dibs/iOS/Dibs/FIRDatabaseController.swift
@@ -0,0 +1,34 @@
+//
+//  FIRDatabaseController.swift
+//  Dibs
+//
+//  Created by Jacob Lattie on 1/23/20.
+//  Copyright © 2020 Jacob Lattie. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+public class FireDatabaseController {
+    
+    var ref: DatabaseReference!
+    
+    init(ref: DatabaseReference) {
+        self.ref = ref
+    }
+    
+    func read() {
+        self.ref.child("chair_id").observeSingleEvent(of: .value, with: { (snapshot) in
+          // Get chair data
+          let value = snapshot.value as? NSDictionary
+          print(value)
+    
+          }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+}
+
+
