17d95af116e5130644e5593521bfc4fac3b5eb82
Jacob Lattie <jhlattie@gatech.edu>
2020-02-27 15:44:16 -0500

    UI background view and Pod support

--- Dibs/iOS/Dibs/IntroViewController.swift
+++ Dibs/iOS/Dibs/IntroViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import Pastel
 
 class IntroViewController: UIViewController, UITextFieldDelegate {
 
@@ -31,6 +32,29 @@ class IntroViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         
         setupUI()
+        
+        let pastelView = PastelView(frame: view.bounds)
+        // Custom Direction
+        pastelView.startPastelPoint = .bottomLeft
+        pastelView.endPastelPoint = .topRight
+
+        // Custom Duration
+        pastelView.animationDuration = 2.0
+
+        // Custom Color
+//        pastelView.setColors([UIColor(red: 156/255, green: 39/255, blue: 176/255, alpha: 1.0),
+//                              UIColor(red: 255/255, green: 64/255, blue: 129/255, alpha: 1.0),
+//                              UIColor(red: 123/255, green: 31/255, blue: 162/255, alpha: 1.0),
+//                              UIColor(red: 32/255, green: 76/255, blue: 255/255, alpha: 1.0),
+//                              UIColor(red: 32/255, green: 158/255, blue: 255/255, alpha: 1.0),
+//                              UIColor(red: 90/255, green: 120/255, blue: 127/255, alpha: 1.0),
+//                              UIColor(red: 58/255, green: 255/255, blue: 217/255, alpha: 1.0)])
+        let colors = [UIColor(red: 252/255, green: 227/255, blue: 138/255, alpha: 1.0),
+                      UIColor(red: 243/255, green: 129/255, blue: 129/255, alpha: 1.0)]
+        pastelView.setColors(colors)
+
+//        pastelView.startAnimation()
+//        view.insertSubview(pastelView, at: 0)
 
         // Do any additional setup after loading the view.
     }
