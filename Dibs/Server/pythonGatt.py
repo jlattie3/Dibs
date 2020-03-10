940dc88eaeb83f1f39ab20b45a8183dd2622db70
Peter Nguyen <42429462+PeterNguyen429@users.noreply.github.com>
2020-03-10 11:47:09 -0400

    Add files via upload

--- Dibs/Server/pythonGatt.py
+++ Dibs/Server/pythonGatt.py
@@ -4,7 +4,7 @@ import pyrebase
 import configparser
 
 configfile = configparser.ConfigParser()
-configfile.read('test.ini')
+configfile.read('DIBS.config')
 
 config = {
   "apiKey": configfile['Configuration']['apiKey'],
@@ -19,27 +19,37 @@ db = firebase.database()
 
 service = DiscoveryService("hci0")
 
-MAC_ADDR = ['FD:B3:DF:4A:A1:02']
+# MAC_ADDR = ['FD:B3:DF:4A:A1:02']
+MAC_ADDR = configfile.sections()
+MAC_ADDR.pop(0)
+print(MAC_ADDR)
 # MAC_ADDR_INFO = {'FD:B3:DF:4A:A1:02': {"status": "", "Building": configfile['FD:B3:DF:4A:A1:02']['Building'], "Floor": configfile['FD:B3:DF:4A:A1:02']['Floor'], "Room" : configfile['FD:B3:DF:4A:A1:02']['Room'], "Chair":configfile['FD:B3:DF:4A:A1:02']['Chair']}}
 # Each chair is associated with a specific MAC Address
 # Preferably put in a config file, so each user does not have access to code
 # FD:B3:DF:4A:A1:02 Bluetooth chip
-
-MAC_ADDR_INFO = {'FD:B3:DF:4A:A1:02': {"status": "", "Building": configfile['FD:B3:DF:4A:A1:02']['Building'], "Floor": configfile['FD:B3:DF:4A:A1:02']['Floor'], "Room" : configfile['FD:B3:DF:4A:A1:02']['Room'], "Chair":configfile['FD:B3:DF:4A:A1:02']['Chair'],
-                                           "Counter": 0 }}
+MAC_ADDR_INFO = {}
+for mac in MAC_ADDR:
+    info = {"status": "0", "Building": configfile[mac]['Building'], "Floor": configfile[mac]['Floor'], "Room" : configfile[mac]['Room'], "Chair":configfile[mac]['Chair'],"Counter": 0 }
+    MAC_ADDR_INFO.update({mac : info})
+print (MAC_ADDR_INFO)
+# MAC_ADDR_INFO = {'FD:B3:DF:4A:A1:02': {"status": "0", "Building": configfile['FD:B3:DF:4A:A1:02']['Building'], "Floor": configfile['FD:B3:DF:4A:A1:02']['Floor'], "Room" : configfile['FD:B3:DF:4A:A1:02']['Room'], "Chair":configfile['FD:B3:DF:4A:A1:02']['Chair'],"Counter": 0 }}
 while True:
     devices = service.discover(2)
     #for address, name in devices.items():
     #    print("name: {}, address: {}".format(name, address))
         
-    configfile.read('test.ini')
-    MAC_ADDR_INFO = {'FD:B3:DF:4A:A1:02': {"status": "", "Building": configfile['FD:B3:DF:4A:A1:02']['Building'], "Floor": configfile['FD:B3:DF:4A:A1:02']['Floor'], "Room" : configfile['FD:B3:DF:4A:A1:02']['Room'], "Chair":configfile['FD:B3:DF:4A:A1:02']['Chair'],
-                                           "Counter": MAC_ADDR_INFO['FD:B3:DF:4A:A1:02']["Counter"]}}
+    configfile.read('DIBS.config')
+#     MAC_ADDR_INFO = {'FD:B3:DF:4A:A1:02': {"status": "0", "Building": configfile['FD:B3:DF:4A:A1:02']['Building'], "Floor": configfile['FD:B3:DF:4A:A1:02']['Floor'], "Room" : configfile['FD:B3:DF:4A:A1:02']['Room'], "Chair":configfile['FD:B3:DF:4A:A1:02']['Chair'],
+#                                            "Counter": MAC_ADDR_INFO['FD:B3:DF:4A:A1:02']["Counter"]}}
+    for mac in MAC_ADDR:
+        info = {"status": "0", "Building": configfile[mac]['Building'], "Floor": configfile[mac]['Floor'], "Room" : configfile[mac]['Room'], "Chair":configfile[mac]['Chair'],"Counter": MAC_ADDR_INFO[mac]["Counter"] }
+        MAC_ADDR_INFO.update({mac : info})
+
     # implement counter
     
     for mac in MAC_ADDR:
+#         if True:
         if mac in devices:
-            
             #Call DB with ID set to 1
             updateValue = {"status": "1"}
             data = MAC_ADDR_INFO.get(mac)
